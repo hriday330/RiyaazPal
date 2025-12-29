d3341cab62fc76b1cc25b9069854f3e046657b57
hriday330 <hriday2004@outlook.com>
2025-12-29 15:42:08 -0800

    Initial Commit

---
+++ RiyaazPal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RiyaazPal
+//
+//  Created by Hriday Buddhdev on 2025-12-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RiyaazPal/RiyaazPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiyaazPalApp.swift
+//  RiyaazPal
+//
+//  Created by Hriday Buddhdev on 2025-12-29.
+//
+
+import SwiftUI
+
+@main
+struct RiyaazPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
