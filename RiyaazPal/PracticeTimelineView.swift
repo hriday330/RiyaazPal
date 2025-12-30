9bcadcfb17ec322c198f564b41ef7d16135dd286
hriday330 <hriday2004@outlook.com>
2025-12-30 11:17:18 -0800

    rename content view to practice timeline view

--- RiyaazPal/PracticeTimelineView.swift
+++ RiyaazPal/PracticeTimelineView.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PracticeTimelineView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world this is Hriday!")
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    PracticeTimelineView()
 }
--- RiyaazPal/RiyaazPalApp.swift
+++ RiyaazPal/RiyaazPalApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RiyaazPalApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            PracticeTimelineView()
         }
     }
 }
