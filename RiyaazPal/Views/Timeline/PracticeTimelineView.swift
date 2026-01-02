1692b77086817da202049ba9150cc4454fe73c7f
hriday330 <hriday2004@outlook.com>
2026-01-01 16:16:41 -0800

    Test build break

--- RiyaazPal/Views/Timeline/PracticeTimelineView.swift
+++ RiyaazPal/Views/Timeline/PracticeTimelineView.swift
@@ -17,7 +17,7 @@ struct PracticeTimelineView: View {
 
                 ScrollView {
                     LazyVStack(spacing: 24) {
-                        ForEach(viewModel.sessionsGroupedByDay, id: \.date) { group in
+                        ForEach(viewModels.sessionsGroupedByDay, id: \.date) { group in
                             DaySection(
                                 date: group.date,
                                 sessions: group.sessions
