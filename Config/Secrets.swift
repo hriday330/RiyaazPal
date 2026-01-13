bc34b1378a02079c8d7fe6f92daf161203549881
hriday330 <hriday2004@outlook.com>
2026-01-12 18:12:53 -0800

    setup connection with supabase edge function

---
+++ Config/Secrets.swift
@@ -0,0 +1,13 @@
+//
+//  SupabaseConfig.swift
+//  RiyaazPal
+//
+//  Created by Hriday Buddhdev on 2026-01-12.
+//
+
+import Foundation
+
+enum Secrets {
+    static let supabaseURL = URL(string: "https://YOUR_PROJECT_URL.supabase.co")!
+    static let supabaseAnonKey = "YOUR_ANON_KEY"
+}
---
+++ RiyaazPal/Views/Insights/ReflectionInsightsSection.swift
@@ -0,0 +1,8 @@
+//
+//  ReflectionInsightsSection.swift
+//  RiyaazPal
+//
+//  Created by Hriday Buddhdev on 2026-01-12.
+//
+
+import Foundation
