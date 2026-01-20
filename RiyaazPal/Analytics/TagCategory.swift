e3354ffc858f92ff2c6e2751bb997aaf3dd58fbb
hriday330 <hriday2004@outlook.com>
2026-01-19 17:51:24 -0800

    Remove old tag registry

--- RiyaazPal/Analytics/TagCategory.swift
+++ RiyaazPal/Analytics/TagCategory.swift
@@ -8,37 +8,6 @@
 import Foundation
 import SwiftData
 
-//struct TagRegistry {
-//
-//    private static let ragas: Set<String> = [
-//        "yaman", "puriya", "bhairav", "todi", "bhimpalasi", "puriya dhanashri", "bilaskhani todi", "ahir bhairav", "darbari kanada", "multani"
-//    ]
-//
-//    private static let sections: Set<String> = [
-//        "alap", "jor", "jhala", "gat",
-//    ]
-//
-//    private static let techniques: Set<String> = [
-//        "meend", "gamak", "kan", "krintan", 
-//    ]
-//
-//    private static let tempos: Set<String> = [
-//        "vilambit", "madhya", "drut"
-//    ]
-//    
-//    private static let taal: Set<String> = [
-//        "teentaal", "jhaptaal", "ektaal", "rupak"
-//    ]
-//
-//    static func category(for tag: String) -> TagCategory {
-//        if sections.contains(tag) { return .section }
-//        if techniques.contains(tag) { return .technique }
-//        if tempos.contains(tag) { return .tempo }
-//        if ragas.contains(tag) { return .raga }
-//        return .other
-//    }
-//}
-
 // NEW TAG CATEGORY
 
 struct TagCategory: Identifiable, Hashable {
